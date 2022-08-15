2dd8c7ebd38b7c2a88f61ff2de4e4e87765d0d6d
PawelPalasinski <pawel.palasinski@gmail.com>
2022-08-16 01:04:06 +0200

    statistics

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -5,26 +5,48 @@ import { Component } from 'react';
 
 // import styles from './FeedbackOptions.module.scss';
 
-
-
-
-
-
 export const FeedbackOptions = () => {
 
-    const [good, setGood] = useState(0);
-    const [neutral, setNeutral] = useState(0);
-    const [bad, setBad] = useState(0);
-    
-    return (
-        <div className="vote-container">
-            <h3 className="vote-container__title">Please leave feedback</h3>
-            <button className="vote-container__grade-btn" onClick={()=> {setGood(good+1)}}>Good</button>
-            <button className="vote-container__grade-btn" onClick={() => { setNeutral(neutral + 1) }}>Neutral</button>
-            <button className="vote-container__grade-btn" onClick={() => { setBad(bad + 1) }}>Bad</button>
-            <span>Good: {good}</span>
-            <span>Neutral: {neutral}</span>
-            <span>Bad: {bad}</span>
-        </div>
-    )
-}
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const totalVotes = good + neutral + bad;
+  const positivePercentage = Math.round((good / totalVotes) * 100);
+
+  return (
+    <div className="vote-container">
+      <h3 className="vote-container__title">Please leave feedback</h3>
+      <button
+        className="vote-container__grade-btn"
+        onClick={() => {
+          setGood(good + 1);
+        }}
+      >
+        Good
+      </button>
+      <button
+        className="vote-container__grade-btn"
+        onClick={() => {
+          setNeutral(neutral + 1);
+        }}
+      >
+        Neutral
+      </button>
+      <button
+        className="vote-container__grade-btn"
+        onClick={() => {
+          setBad(bad + 1);
+        }}
+      >
+        Bad
+      </button>
+
+      <h3 className="vote-container__statistics">Statistics</h3>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>Total: {totalVotes}</p>
+      <p>Positive feedback: {positivePercentage}%</p>
+    </div>
+  );
+};
