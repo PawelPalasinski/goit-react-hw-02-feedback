5cf1895b58c9ad81dffbb610e5d23596f540c30a
PawelPalasinski <pawel.palasinski@gmail.com>
2022-08-17 00:54:47 +0200

    App.jsx components structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,38 @@
-import { FeedbackOptionsV1 } from "./FeedbackOptionsV1/FeedbackOptionsV1";
+import { FeedbackOptionsV1 } from './FeedbackOptionsV1/FeedbackOptionsV1';
 
-export const App = () => {
-  return (
-    <div>
-      <FeedbackOptionsV1 />
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import Statistics from './Statistics/Statistics';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Section from './Section/Section';
+import Notification from './Notification/Notification';
+
+// export const App = () => {
+//   return (
+//     <div>
+//       <FeedbackOptionsV1 />
+//     </div>
+//   );
+// };
+
+export class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+      <>
+        <Section title="Please leave feedback">
+          <FeedbackOptions />
+        </Section>
+
+        <Section title="Statistics"></Section>
+      </>
+    );
+  }
+}
+
+export default App;
